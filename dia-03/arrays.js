3b1a5d987bc731819ce86d1aef8b0122efdc4364
Matheus Wolf <matheus_wolf@hotmail.com>
2026-05-25 13:37:24 -0300

    dia 3: forEach, map, filter, reduce e encadeamento

--- dia-03/arrays.js
+++ dia-03/arrays.js
@@ -1,11 +1,15 @@
-const frutas = ["maçã", "banana", "uva", "laranja"];
+const produtos = [
+    { nome: "Notebook", preco: 3000, categoria: "eletrônico" },
+    { nome: "Celular", preco: 2000, categoria: "eletrônico" },
+    { nome: "Fone", preco: 500, categoria: "eletrônico" },
+    { nome: "Camiseta", preco: 80, categoria: "roupa" },
+    { nome: "Calça", preco: 200, categoria: "roupa" },
+    { nome: "Tênis", preco: 350, categoria: "roupa" }
+];
 
-console.log("--- Com for...of ---");
-for (const fruta of frutas) {
-    console.log(fruta);
-}
+const total = produtos
+    .filter((p) => p.categoria === "eletrônico")
+    .map((p) => p.preco * 0.9)
+    .reduce((acc, p) => acc + p , 0);
 
-console.log("--- Com forEach ---");
-frutas.forEach((fruta) => {
-    console.log(fruta);
-});
+console.log("Total com desconto:", total);
