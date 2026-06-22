7d3fac9126717dd4cfe0cdb59ffbc5e19c2acc8b
Matheus Wolf <matheus_wolf@hotmail.com>
2026-06-22 16:27:55 -0300

    Dia 10-11: TypeScript - tipos em variaveis e funcoes

---
+++ dia-10/funcoes.ts
@@ -0,0 +1,11 @@
+function somar(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(somar (7, 4))
+
+function saudacao(nome: string): string {
+   return "Olá, " + nome + "!";
+}
+
+console.log(saudacao("Matheus"));
---
+++ dia-10/tipos.ts
@@ -0,0 +1,5 @@
+let nome: string = "Matheus";
+let idade: number = 30;
+let ativo: boolean = true;
+
+console.log(nome, idade, ativo);
